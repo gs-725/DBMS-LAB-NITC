7c27b1ca3959edede6c07a37d6b16d72f0ac282f
gs-725 <gsoneesh725@gmail.com>
2026-01-06 20:25:14 +0530

    Stage2 Exercise Q1

--- mynitcbase/main.cpp
+++ mynitcbase/main.cpp
@@ -10,15 +10,15 @@ int main(int argc, char *argv[]) {
 
   // create objects for the relation catalog and attribute catalog
   RecBuffer relCatBuffer(RELCAT_BLOCK);
-  RecBuffer attrCatBuffer(ATTRCAT_BLOCK);
+  
 
   HeadInfo relCatHeader;
-  HeadInfo attrCatHeader;
+  
 
   // load the headers of both the blocks into relCatHeader and attrCatHeader.
   // (we will implement these functions later)
   relCatBuffer.getHeader(&relCatHeader);
-  attrCatBuffer.getHeader(&attrCatHeader);
+ 
 
   for (int i=0;i<relCatHeader.numEntries;i++/* i = 0 to total relation count */) {
 
@@ -27,7 +27,13 @@ int main(int argc, char *argv[]) {
     relCatBuffer.getRecord(relCatRecord, i);
 
     printf("Relation: %s\n", relCatRecord[RELCAT_REL_NAME_INDEX].sVal);
-
+    int count=relCatRecord[RELCAT_NO_ATTRIBUTES_INDEX].nVal;
+    int blockNum=ATTRCAT_BLOCK;
+    while(count!=0){
+    
+	RecBuffer attrCatBuffer(blockNum);
+	HeadInfo attrCatHeader;
+	attrCatBuffer.getHeader(&attrCatHeader);
     for (int j=0;j<attrCatHeader.numEntries;j++/* j = 0 to number of entries in the attribute catalog */) {
 
       // declare attrCatRecord and load the attribute catalog entry into it
@@ -37,8 +43,11 @@ int main(int argc, char *argv[]) {
       if (strcmp(attrCatRecord[ATTRCAT_REL_NAME_INDEX].sVal, relCatRecord[RELCAT_REL_NAME_INDEX].sVal) == 0/* attribute catalog entry corresponds to the current relation */) {
         const char *attrType = attrCatRecord[ATTRCAT_ATTR_TYPE_INDEX].nVal == NUMBER ? "NUM" : "STR";
         printf("  %s: %s\n", attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal/* get the attribute name */, attrType);
+        count--;
       }
     }
+  blockNum=attrCatHeader.rblock;
+  }
     printf("\n");
   }
 
