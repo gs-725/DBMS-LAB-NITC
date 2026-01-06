35e85d2308111e9eab30caddbe188f5063e1fc64
gs-725 <gsoneesh725@gmail.com>
2026-01-06 20:08:43 +0530

    before exercise functions completion

--- mynitcbase/main.cpp
+++ mynitcbase/main.cpp
@@ -6,19 +6,41 @@
 #include<iostream>
 
 int main(int argc, char *argv[]) {
-  /* Initialize the Run Copy of Disk */
   Disk disk_run;
- 
 
-   unsigned char buffer[BLOCK_SIZE];// this means the buffer is containing 2048 bytes we have to access each byte and do int cast and also this is just the block 0 first 10 bytes actually for Block allocation map block 0-3 is allocated so there is 4*2048 bytes
-   Disk::readBlock(buffer, 0);
-  for(int j=0;j<10;j++)
-  	std::cout <<int(buffer[j])<<std::endl;
-  			
+  // create objects for the relation catalog and attribute catalog
+  RecBuffer relCatBuffer(RELCAT_BLOCK);
+  RecBuffer attrCatBuffer(ATTRCAT_BLOCK);
+
+  HeadInfo relCatHeader;
+  HeadInfo attrCatHeader;
+
+  // load the headers of both the blocks into relCatHeader and attrCatHeader.
+  // (we will implement these functions later)
+  relCatBuffer.getHeader(&relCatHeader);
+  attrCatBuffer.getHeader(&attrCatHeader);
+
+  for (int i=0;i<relCatHeader.numEntries;i++/* i = 0 to total relation count */) {
+
+    Attribute relCatRecord[RELCAT_NO_ATTRS]; // will store the record from the relation catalog
+
+    relCatBuffer.getRecord(relCatRecord, i);
+
+    printf("Relation: %s\n", relCatRecord[RELCAT_REL_NAME_INDEX].sVal);
+
+    for (int j=0;j<attrCatHeader.numEntries;j++/* j = 0 to number of entries in the attribute catalog */) {
+
+      // declare attrCatRecord and load the attribute catalog entry into it
+      Attribute attrCatRecord[ATTRCAT_NO_ATTRS];
+      attrCatBuffer.getRecord(attrCatRecord,j);
+
+      if (strcmp(attrCatRecord[ATTRCAT_REL_NAME_INDEX].sVal, relCatRecord[RELCAT_REL_NAME_INDEX].sVal) == 0/* attribute catalog entry corresponds to the current relation */) {
+        const char *attrType = attrCatRecord[ATTRCAT_ATTR_TYPE_INDEX].nVal == NUMBER ? "NUM" : "STR";
+        printf("  %s: %s\n", attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal/* get the attribute name */, attrType);
+      }
+    }
+    printf("\n");
+  }
 
   return 0;
-  // StaticBuffer buffer;
-  // OpenRelTable cache;
-
-  //return FrontendInterface::handleFrontend(argc, argv);
 }
